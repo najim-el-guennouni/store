c3c888f15bc900a4c1d55fae2bd9dbcfe18737bb
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2022-06-30 00:01:33 +0100

    add product part 2

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,14 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    private $productRepository;
+    private $entityManager;
+    public function __construct(ProductRepository $productRepository, ManagerRegistry $doctrine)
+    {
+        $this->productRepository = $productRepository;
+        $this->entityManager = $doctrine->getManager();
+    }
+
+
+    #[Route('/product', name: 'product_list')]
     public function index(): Response
     {
         return $this->render('product/index.html.twig', [
@@ -23,6 +34,30 @@ class ProductController extends AbstractController
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            if ($request->files->get('product')['image']) {
+                $image = $request->files->get('product')['image'];
+                $image_name = time() . '_' . $image->getClientOriginalName();
+                // Move the file to the directory where brochures are stored
+                $image->move(
+                    $this->getParameter('image_directory'),
+                    $image_name
+                );
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $product->setImage($image_name);
+            }
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Your product was saved'
+            );
+            return $this->redirectToRoute('product_list');
+        }
         return $this->renderForm('product/create.html.twig', [
             'form' => $form
         ]);
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProductType extends AbstractType
 {
@@ -28,7 +29,8 @@ class ProductType extends AbstractType
                 [
                     'class' => Category::class
                 ]
-            );
+            )
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
