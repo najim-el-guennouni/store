efde935bf3a311e8c6269e26c609931fd5eaf846
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2022-07-02 17:05:41 +0100

    Sing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    private $productRepository;
+    private $CategoryRepository;
+    private $entityManager;
+    public function __construct(ProductRepository $productRepository, ManagerRegistry $doctrine, CategoryRepository $CategoryRepository)
+    {
+        $this->productRepository = $productRepository;
+        $this->CategoryRepository = $CategoryRepository;
+        $this->entityManager = $doctrine->getManager();
+    }
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        $products = $this->productRepository->findAll();
+        $categories = $this->CategoryRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
+            'categories' => $categories,
+        ]);
+    }
+    #[Route('/produc/{category}', name: 'product_category')]
+    public function categoryProducts(Category $category): Response
+    {
+        $categories = $this->CategoryRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'products' => $category->getProducts(),
+            'categories' => $categories,
         ]);
     }
 }
