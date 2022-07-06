9684fec22b1813f73e472525383a74976417fda7
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2022-07-06 11:23:58 +0100

    Secure the app & 404 Pag

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,8 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+
+        
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
